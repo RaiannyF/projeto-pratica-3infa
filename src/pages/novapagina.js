1485ca9f30779db02e6edfe540f4024caec08ae7
Pudimkak <dosreisgabriel49@gmail.com>
2022-05-20 15:37:03 -0300

    criação de nova página

---
+++ src/pages/novapagina.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Quando você olha muito tempo para o abismo, o abismo olha de volta pra você</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
